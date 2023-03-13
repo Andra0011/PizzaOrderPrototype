339ba79e2c9d05b76c4dbd6885759fac386fb4a5
Andra0011 <capraruandra@gmail.com>
2023-03-13 16:35:55 +0200

    fix data json

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,34 +1,35 @@
 
 const addEl = (
-    type,
-    parent,
-    atr1,
-    atr1Name,
-    atr2,
-    atr2Name,
-    atr3,
-    atr3Name
-  ) => {
-    let el = document.createElement(type);
-    if (atr1 != undefined) el.setAttribute(atr1, atr1Name);
-    if (atr2 != undefined) el.setAttribute(atr2, atr2Name);
-    if (atr3 != undefined) el.setAttribute(atr3, atr3Name);
-    if (parent != undefined) parent.appendChild(el);
-    return el;
-  };
+  type,
+  parent,
+  atr1,
+  atr1Name,
+  atr2,
+  atr2Name,
+  atr3,
+  atr3Name
+) => {
+  let el = document.createElement(type);
+  if (atr1 != undefined) el.setAttribute(atr1, atr1Name);
+  if (atr2 != undefined) el.setAttribute(atr2, atr2Name);
+  if (atr3 != undefined) el.setAttribute(atr3, atr3Name);
+  if (parent != undefined) parent.appendChild(el);
+  return el;
+};
 
-// document.querySelector("body").insertAdjacentHTML("beforeend", `<div id = "root"></div>`);
-const rootElement = document.querySelector("#root");
 
 const loadAPI = async () => {
-    let pizzaList = await fetch("/api/pizza");
-    let pizzaJSON = pizzaList.json();
-    console.log(pizzaJSON)
+  let pizzaList = await fetch(`http://127.0.0.1:9001/api/pizza`);
+  let pizzaJSON = await pizzaList.json();
+  console.log(pizzaJSON)
+  addEl("div", root, "id", "pizzaJSON")
+  const allPizza = document.getElementById("pizzaJSON")
+  allPizza.insertAdjacentHTML("beforeend", `<div>${pizzaJSON[0].name}</div>`)
+
 }
 
 const loadEvent = () => {
-    loadAPI();
-    addEl("div", root, "id", "pizzaJSON")
+  loadAPI();
 
 };
 
