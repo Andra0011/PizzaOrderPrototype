06a3e2e6b6f91d8ad5b75f1ac1df0c56c9a1d2bc
Teodor <teodormihai2000@gmail.com>
2023-03-15 16:55:39 +0200

    filter-checkboxes-working*

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -44,7 +44,7 @@ let createAllergensCheckers = (allergen) => {
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.id = `${allergen.name}Filter`;
-  checkbox.className="checkboxx"
+  checkbox.className = "checkboxx"
   checkbox.value = `${allergen.name}`;
 
   const label = document.createElement("label");
@@ -64,64 +64,50 @@ const loadAPI = async () => {
   let allergensList = await fetch(`http://127.0.0.1:9001/api/allergen`);
   pizzaJSON = await pizzaList.json();
   allergensJSON = await allergensList.json();
-  // const counterArr = Array(9).fill(0);
-  
+
   allergensJSON.forEach(allergen => {
     createAllergensCheckers(allergen);
     const checkbox = document.querySelector(`#${allergen.name}Filter`)
-    checkbox.textContent = `${allergen.name}`
-    
-    checkbox.addEventListener("change", (e) => {
-      let checkedArr=[], allergenArr=[]
+
+    checkbox.addEventListener("change", () => {
+      let checkedArr = [], allergenArr = []
       const pizzasToFilter = document.querySelectorAll(`.${allergen.name}`)
       const allCheckboxes = document.querySelectorAll(".checkboxx")
       const allPizzas = document.querySelectorAll(".pizza")
       allCheckboxes.forEach(checkboxx => {
-        if(checkboxx.checked){
+        if (checkboxx.checked) {
           checkedArr.push(checkboxx.value)
         }
       })
-      
-      // counterArr[allergen.id - 1]++;
-      // console.log(counterArr)
+
       pizzasToFilter.forEach(pizzaToFilter => {
-        
-        // let allergenNamesArr=[]
         pizzaJSON.forEach(pizza => {
           const allergenString = document.querySelector(`#allergenList${pizza.id}`)
           if (pizzaToFilter.id == `Pizza${pizza.id}`) {
             allergenArr = allergenString.textContent.split(", ")
-            // console.log(pizzaToFilter)
-            // allergenNamesArr = allergenIdToName(pizza.allergens).split(", ")
-            // if (allergenNamesArr.includes(checkbox.textContent)) {
-              //   console.log(checkbox.textContent)
-              // }
+          }
+        })
+        console.log(allergenArr, checkedArr)
+        allPizzas.forEach(pizza => {
+          pizza.classList.remove('hidden')
+          checkedArr.forEach(checked => {
+            if (pizza.classList.contains(checked)) {
+              pizza.classList.add('hidden');
             }
           })
-          console.log(allergenArr, checkedArr)
-          // let commonAllergens=checkedArr.filter(allergen => allergenArr.includes(allergen))
-          // console.log(commonAllergens)
-          allPizzas.forEach(pizza => {
-            // console.log(pizza)
-            pizza.classList.remove('hidden')
-            checkedArr.forEach(checked => {
-              if (pizza.classList.contains(checked)) {
-                pizza.classList.add('hidden');
-              }
-            })
-          })
-          
+        })
       });
-      console.log("end")
+
+      console.log("end");
     })
   });
 
 
 
   addEl("div", root, "id", "pizzaJSON")
-  const allPizza = document.getElementById("pizzaJSON")
   pizzaJSON.forEach(pizza => {
-    allPizza.insertAdjacentHTML("beforeend", `<div id="Pizza${pizza.id}" class="pizza ${allergenIdToDivClasses(pizza.allergens)}">${pizza.name} : </div>`)
+    const allPizzaDiv = document.getElementById("pizzaJSON")
+    allPizzaDiv.insertAdjacentHTML("beforeend", `<div id="Pizza${pizza.id}" class="pizza ${allergenIdToDivClasses(pizza.allergens)}">${pizza.name} : </div>`)
     const thatPizza = document.getElementById(`Pizza${pizza.id}`)
     thatPizza.insertAdjacentHTML("beforeend", `<div id="allergenList${pizza.id}">${allergenIdToName(pizza.allergens)}<br/><br/></div>`)
   });
