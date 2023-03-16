57f715d1d611148430b9d28a34f0b6c85d0dae96
Andra0011 <capraruandra@gmail.com>
2023-03-16 10:02:18 +0200

    style css

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -21,11 +21,46 @@ const addEl = (
 const loadAPI = async () => {
   let pizzaList = await fetch(`http://127.0.0.1:9001/api/pizza`);
   let pizzaJSON = await pizzaList.json();
-  console.log(pizzaJSON)
-  addEl("div", root, "id", "pizzaJSON")
-  const allPizza = document.getElementById("pizzaJSON")
-  allPizza.insertAdjacentHTML("beforeend", `<div>${pizzaJSON[0].name}</div>`)
+  // console.log(pizzaJSON)
+  let allergensList = await fetch(`http://127.0.0.1:9001/api/allergen`);
+  let allergensJSON = await allergensList.json();
+  // console.log(allergensJSON)
 
+  addEl("div", root, "class", "allergens")
+  addEl("div", root, "id", "pizzaJSON")
+
+  const filterBtn = document.querySelector(".filter")
+  const allPizzaDiv = document.getElementById("pizzaJSON")
+  const allAllergens = document.querySelector(".allergens")
+  allPizzaDiv.insertAdjacentHTML("beforeend", `<div id= "pizza">
+  <p id="pizzaName">${pizzaJSON[0].name}</p>
+  <p id="pizzaDetails">Detalii Pizza</p>
+  <p id="pizzaPrice">35,00 lei</p>
+  </div>`)
+  const pizzas = document.querySelector("#pizza")
+  console.log(pizzas)
+  allAllergens.insertAdjacentHTML("beforeend", `<div id="allergen">${allergensJSON[0].name}</div> 
+  <label class="container">
+  <input checked="checked" type="checkbox">
+  <div class="checkmark"></div>
+</label> `)
+
+  filterBtn.addEventListener("click", (e) => {
+    console.log(allAllergens.style.visibility)
+    if(allAllergens.style.visibility == "visible"){
+      allAllergens.style.transition = "height 0s"
+      allAllergens.style.visibility = "hidden"
+      allAllergens.style.height = "5vh"
+      pizzas.style.marginLeft = "200px"
+
+
+    } else {
+      allAllergens.style.transition = "height 3s"
+      allAllergens.style.visibility = "visible"
+      allAllergens.style.height = "100vh"
+      pizzas.style.marginLeft = "400px"
+    }
+  })
 }
 
 const loadEvent = () => {
